899bec7ebc616f6076c6037bb81762202bb9b653
cao sheng <a0077952@nus.edu.sg>
2013-10-22 05:05:11 +0800

    done with basic functions

--- testcut1.py
+++ testcut1.py
@@ -2,6 +2,8 @@ import maya.cmds as mc
 import maya.mel as mm
 import random
 
+kMoveAwayXDistance = 20
+
 def form_cutting_planes_for_object(obj, num):
 	"num = number of separations in each direction, result in num - 1 planes"
 	res = []
@@ -31,22 +33,27 @@ def form_cutting_planes_for_object(obj, num):
 	return res
 
 
-
-def cut_object_with_planes_and_ratios(obj, planes, ratios, threshold):
-	volume_total = mm.eval('meshVolume(\"' + obj + '\")')
+# input: one object, total volume, generated cutting planes, list of ratios interested, error threshold
+# return: on success, return a list of separated parts moved kMoveAwayXDistance in x direction
+#	on failure, return a dictionary contains key 'bad_cut_objs' -> list of object that cannot 
+#	match any ratios, and 'ratios_remaining' -> list of remaining ratios, 
+# 	and 'good_cut_objs' -> list of objects found
+def cut_object_with_planes_and_ratios(obj, volume_total, planes, ratios, threshold):
+	# create a list of booleans indicating ratios found or not
 	ratio_found = []
 	for r in ratios: ratio_found.append(0)
+	ratios.sort()
 	results = []
+	# a list of objects that volume cannot be match anymore
+	bad_cut_objs = []
 	all_found = False
 	# initially we have only one object to cut
 	objs_to_cut = [obj]
 
 	# loop all cut planes
 	for plane in planes:
-		#print 'cut use plane: '
-		#print plane
 		# store all object result from this cut
-		objs_for_next_iteration = []
+		objs_for_next_plane_iteration = []
 		# for each object in world
 		for i in range(len(objs_to_cut)):
 			#print 'cut object: ' + objs_to_cut[i]
@@ -57,57 +64,111 @@ def cut_object_with_planes_and_ratios(obj, planes, ratios, threshold):
 			mc.select(objs_to_cut[i], r = True)
 			mc.polyCloseBorder()
 			# separate
-			# if number of pieces < 2, means the plane and the object did not intersect
-			if mc.polyEvaluate(shell = True) < 2:
+			# if number of pieces < 2, means the plane and the object did not have intersection
+			if mc.polyEvaluate(objs_to_cut[i], shell = True) < 2:
 				# add back this object
-				objs_for_next_iteration.append(objs_to_cut[i])
+				objs_for_next_plane_iteration.append(objs_to_cut[i])
 				# continue with other objs_to_cut
 				continue
 			parts = mc.polySeparate(objs_to_cut[i])
-			# add parts to future objs to cut
-			objs_for_next_iteration.extend(parts[0:-1])
+			# add these parts to future objs to cut
+			objs_for_next_plane_iteration.extend(parts[0:-1])
 			# for each parts
 			for j in range(len(parts) - 1):
 				this_volume_ratio = mm.eval('meshVolume(\"' + parts[j] + '\")') / volume_total
 				#print 'volume: ', this_volume_ratio
 				# check volume
+				first_unfound_volume = True
 				for k in range(len(ratios)):
-					if ratio_found[k] == 0 and abs(this_volume_ratio - ratios[k]) < threshold:
-						print 'volume found: ', this_volume_ratio
-						# dup the object
-						temp = mc.duplicate(parts[j])
-						mc.select(temp[0], r = True)
-						# move away the duplication
-						mc.move(20, 0, 0, temp[0])
-						# add it to the result list
-						results.append(temp[0])
-						# remove the current object
-						mc.delete(parts[j])
-						objs_for_next_iteration.remove(parts[j])
-						# mark volume as found
-						ratio_found[k] = 1
-						# if all parts found
-						if ratio_found.count(0) == 0:
-							all_found = True
-						break
+					if ratio_found[k] == 0:
+						# this part's volume is less than the smallest volume unfound
+						if first_unfound_volume and this_volume_ratio + threshold < ratios[k]:
+							print 'bad volume found, save', this_volume_ratio
+							objs_for_next_plane_iteration.remove(parts[j])
+							bad_cut_objs.append(parts[j])
+							break
+						# got match
+						elif abs(this_volume_ratio - ratios[k]) < threshold:
+							print 'volume found: ', this_volume_ratio
+							# dup the object
+							temp = mc.duplicate(parts[j])
+							mc.select(temp[0], r = True)
+							# move away the duplication
+							mc.move(kMoveAwayXDistance, 0, 0, temp[0])
+							# add it to the result list
+							results.append(temp[0])
+							# remove the current object
+							mc.delete(parts[j])
+							objs_for_next_plane_iteration.remove(parts[j])
+							# mark volume as found
+							ratio_found[k] = 1
+							# if all parts found
+							if ratio_found.count(0) == 0:
+								all_found = True
+							break
+						if first_unfound_volume:
+							first_unfound_volume = False
 				if all_found: break
 			if all_found: break
-		objs_to_cut = objs_for_next_iteration
+		objs_to_cut = objs_for_next_plane_iteration
 		if all_found:
 			# todo move back all result obj
 			print 'FFFFFFFFFFFFFFFFFFFFFFFFUUUUUUUUUUUUUUUUUUUUUUUUUU'
 			return results
+		elif len(objs_to_cut) == 0:
+			# no more cuttings but not all_found
+			break
 
-	return False
+	# objs_to_cut might be empty due to insufficient planes OR empty
+	if len(objs_to_cut) != 0:
+		bad_cut_objs.extend(objs_to_cut)
+	ratios_remaining = []
+	for f in ratio_found:
+		if f == 0:
+			ratios_remaining.append(ratios[f])
+	return {'bad_cut_objs': bad_cut_objs, 'ratios_remaining': ratios_remaining, 'good_cut_objs': results}
 
 
 mc.select(all=True)
 mc.delete()
 cube1 = mc.polyCube(sx=1, sy=1, sz=1, h=5, w=5, d=5)
-#print mc.objectCenter(cube1[0])
-cut_planes = form_cutting_planes_for_object(cube1[0], 4)
-random.shuffle(cut_planes)
+object_name = cube1[0]
+volume_total = mm.eval('meshVolume(\"' + object_name + '\")')
 volume_ratios = [0.25, 0.5, 0.25]
-print cut_object_with_planes_and_ratios(cube1[0], cut_planes, volume_ratios, 0.05)
+#print mc.objectCenter(cube1[0])
+# LOOP A
+cut_planes = form_cutting_planes_for_object(object_name, 4)
+# LOOP B
+random.shuffle(cut_planes)
+# LOOP C
+result_from_cutting = cut_object_with_planes_and_ratios(object_name, volume_total, cut_planes, volume_ratios, 0.05)
+if isinstance(result_from_cutting, list):
+	# this list contains all successfully cut objects and we are done
+	print 'DONE!'
+else:
+	# dictionary returned
+	bad_cut_objs = result_from_cutting['bad_cut_objs']
+	ratios_remaining = result_from_cutting['ratios_remaining']
+	good_cut_objs = result_from_cutting['good_cut_objs']
+	# combine and merge all remaining parts
+	united_objects = mc.polyUnite(bad_cut_objs)[0]
+	mc.polyMergeVertex(united_objects)
+	if mc.polyEvaluate(united_objects, shell = True) > 1:
+		# remaining parts cannot be merged into one
+		print 'NO REDEMPTION'
+	else:
+		if len(ratios_remaining) == 1:
+			# only one ratio remains, must be what we need
+			temp = mc.duplicate(united_objects)
+			mc.select(temp[0], r = True)
+			# move away the duplication
+			mc.move(kMoveAwayXDistance, 0, 0, temp[0])
+			# remove the current object
+			mc.delete(united_objects)
+			print 'DONE with merge!'
+		else:
+			# LOOP D
+			print 'continue with LOOP D'
+	print 'FUCK!'
 
 
