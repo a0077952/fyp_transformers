b8e8ac7b4dbf1ba1987f2432c4b4780327fa234b
CAO SHENG <caoshengcc@gmail.com>
2014-02-03 19:25:18 +0800

    fuck

--- testcut2.py
+++ testcut2.py
@@ -9,10 +9,18 @@ cube1 = mc.polyCube(sx=1, sy=1, sz=1, h=3, w=3, d=3)
 #cube1 = mc.polyCylinder(r=3, h=5, sx=20, sy=1, ax=[0,1,0])
 #cube1 = mc.polyCone(r=1, h=2, ax=[0,1,0])
 #cube1 = mc.polyPyramid()
-object_name = cube1[0]
+obj = cube1[0]
 part0 = [2, 2, 3, 1, 2]
 part1 = [1, 2, 3, 0]
 part2 = [1, 2, 3, 0]
 part3 = [1, 1, 3, 0]
 
-# select part 0
+bbox = mc.exactWorldBoundingBox(obj)
+print 'bbox', bbox
+
+# yz
+pyz = {'pc': [part0[0], 0, 0], 'ro': [0, 90, 0]}
+# xz
+pxz = {'pc': [0, part0[1], 0], 'ro': [-90, 0, 0]}
+# xy
+pxy = {'pc': [0, 0, part0[2]], 'ro': [0, 0, 0]}
