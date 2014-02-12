a0b63bea9b0e86b52a992c89bca05f78c69d4ab7
cao sheng <a0077952@nus.edu.sg>
2014-02-13 02:53:01 +0800

    backtrack tree cut

--- testcut2.py
+++ testcut2.py
@@ -1,27 +1,6 @@
-import maya.cmds
-import maya.mel
+import maya.cmds as cmds
+import maya.mel as mel
 import random 
-
-# part is defined by bbox and center
-# def cut_part(mesh, bbox, center):
-# 	cutmesh = cmds.duplicate(mesh)
-# 	#cutmesh = mesh
-# 	minx, miny, minz, maxx, maxy, maxz = bbox
-# 	cx, cy, cz = center
-# 	cmds.select(cutmesh)
-# 	cmds.polyCut(pc = (minx + cx, 0, 0), ro = (0, 90,0), df =1, ch=0 )
-# 	cmds.polyCloseBorder(ch=0)
-# 	cmds.polyCut(pc = (maxx + cx, 0, 0), ro = (0, -90,0), df = 1, ch=0)
-# 	cmds.polyCloseBorder(ch=0)
-# 	cmds.polyCut(pc = (0, 0, minz + cz), ro = (0, 0,0), df =1, ch=0 )
-# 	cmds.polyCloseBorder(ch=0)
-# 	cmds.polyCut(pc = (0, 0, maxz + cz), ro = (0, 180,0), df = 1, ch=0)
-# 	cmds.polyCloseBorder(ch=0)
-# 	cmds.polyCut(pc = (0, miny + cy, 0), ro = (-90, 0,0), df =1,ch=0 )
-# 	cmds.polyCloseBorder(ch=0)
-# 	cmds.polyCut(pc = (0, maxy + cy, 0), ro = (90, 0,0), df = 1, ch=0)
-# 	cmds.polyCloseBorder(ch=0)
-# 	cmds.select(cutmesh)
 
 # create a cuboid, return its name
 def create_part(part):
@@ -42,72 +21,59 @@ def move_obj_center(obj, new_center):
 	cmds.move(translation[0], translation[1], translation[2], obj, r=1)
 	return cmds.objectCenter(obj)
 
-
-# minx, miny, minz, maxx, maxy, maxz = bbox
-# cx, cy, cz = c
-# cmds.select(mesh)
-# cmds.polyCut(pc = (minx + cx, 0, 0), ro = (0, 90,0), ef=1)
-# cmds.polyCloseBorder()
-# cmds.polyCut(pc = (maxx + cx, 0, 0), ro = (0, -90,0), ef=1)
-# cmds.polyCloseBorder()
-# cmds.polyCut(pc = (0, 0, minz + cz), ro = (0, 0,0), ef=1)
-# cmds.polyCloseBorder()
-# cmds.polyCut(pc = (0, 0, maxz + cz), ro = (0, 180,0), ef=1)
-# cmds.polyCloseBorder()
-# cmds.polyCut(pc = (0, miny + cy, 0), ro = (-90, 0,0), ef=1)
-# cmds.polyCloseBorder()
-# cmds.polyCut(pc = (0, maxy + cy, 0), ro = (90, 0,0), ef=1)
-# cmds.polyCloseBorder()
-# cmds.select(cutmesh)
-
 def volume(mesh):
-	return mm.eval('meshVolume(\"' + mesh + '\")')
+	return mel.eval('meshVolume(\"' + mesh + '\")')
 
 def rec(remaining, part, move):
 	print 'enter step', move
-	cmds.select(remaining, r=1)
-	cmds.polyCloseBorder()
 
 	# last part
 	if len(part) == 1: 
-		print 'last part!'
-		vol_rem = volume(remaining)
-		vol_part = volume(part)
-		ratio = vol_rem / vol_part
-		if ratio < 1.5 and ratio > 0.6:
-			return True
-		else:
-			return False
+		print 'done'
+		return True
+		# print 'last part!'
+		# vol_rem = volume(remaining)
+		# vol_part = volume(part)
+		# ratio = vol_rem / vol_part
+		# if ratio < 15 and ratio > 0.6:
+		# 	print 'last part going to return true'
+		# 	return True
+		# else:
+		# 	print 'last part going to return false'
+		# 	cmds.delete(remaining)
+		# 	return False
 
-
-	# 10 tries each depth
-	# for i in range(10):
-		# select a part (1st one)
-	cutpart = create_part(part[0])
-	vol_part = volume(cutpart)
-	for j in range(10):
-		# 10 tries for a suitable part
+	backup_rem = cmds.duplicate(remaining)
+	for j in range(100):
+		# 100 tries for a suitable part
+		cutpart = create_part(part[0])
+		vol_part = volume(cutpart)
 		cutcenter = rand_center(remaining)
 		move_obj_center(cutpart, cutcenter)
-		dup_rem = cmds.duplicate(remaining)
-		dup_cut = cmds.duplicate(cutpart)
 
+		new_cut = cmds.polyBoolOp(cmds.duplicate(remaining), cmds.duplicate(cutpart), op=3)[0]
 		new_rem = cmds.polyBoolOp(remaining, cutpart, op=2)[0]
-		new_cut = cmds.polyBoolOp(dup_rem, dup_cut, op=3)[0]
 		vol_cut = volume(new_cut)
-		if vol_cut / vol_part > 0.7:
+		if vol_cut / vol_part > 0.9:
 			# this part might work
-			move = move + 3
-			result = rec(new_rem, part[1:], move)
+			result = rec(new_rem, part[1:], move + 3)
 			if result == True:
-				print 'fuck', new_cut
+				print 'sol found, return to upper lv'
 				cmds.move(move, 0, 0, new_cut, r=1)
+				cmds.delete(backup_rem)
 				return True
+			else:
+				cmds.delete(new_cut)
+				remaining = cmds.duplicate(backup_rem)
+				print 'shit try again'
+		else:
+			cmds.delete(new_cut)
+			cmds.delete(new_rem)
+			remaining = cmds.duplicate(backup_rem)
 
-		remaining = cmds.polyBoolOp(new_rem, new_cut, op=1)[0]
-		cmds.select(remaining, r=1)
-		cmds.polyCloseBorder()
-		cutpart = create_part(part[0])
+	print 'oh no fail 100 times in step', move
+	cmds.delete(backup_rem)
+	cmds.delete(remaining)
 	return False
 
 	
@@ -121,7 +87,7 @@ part1 = [-0.5, -1, -1.5, 0.5, 1, 1.5]
 part2 = [-0.5, -1, -1.5, 0.5, 1, 1.5]
 part3 = [-0.5, -0.5, -1.5, 0.5, 0.5, 1.5]
 part = [part0, part1, part2, part3]
-rec(remaining, part, 5)
+rec(remaining, part[:4], 5)
 
 # cutpart = create_part(part0)
 # vp = volume(cutpart)
