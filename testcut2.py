73b711d2846377b0150e1331de6c16082f580df5
CAO SHENG <caoshengcc@gmail.com>
2014-02-20 10:24:08 +0800

    'fuck'

--- testcut2.py
+++ testcut2.py
@@ -24,8 +24,28 @@ def move_obj_center(obj, new_center):
 def volume(mesh):
 	return mel.eval('meshVolume(\"' + mesh + '\")')
 
+# return number of vertices of bbox2 outside bbox1
+def vertices_outside(bbox1, bbox2):
+	minx, miny, minz, maxx, maxy, maxz = bbox1
+	cminx, cminy, cminz, cmaxx, cmaxy, cmaxz = bbox2
+
+	def vertex_inside_bbox(v):
+		xin = v[0] > minx and v[0] < maxx
+		yin = v[1] > miny and v[1] < maxy
+		zin = v[2] > minz and v[2] < maxz
+		return xin and yin and zin
+
+	sum = 0
+	# 8 vertices of cutpart's bbox
+	for x in [cminx, cmaxx]:
+		for y in [cminy, cmaxy]:
+			for z in [cminz, cmaxz]:
+				sum = sum + vertex_inside_bbox([x, y, z])
+
+	return 8 - sum
+
 def rec(remaining, part, move):
-	print 'enter step', move
+	print 'enter step', (move-5)/3
 
 	# last part
 	if len(part) == 1: 
@@ -44,17 +64,29 @@ def rec(remaining, part, move):
 		# 	return False
 
 	backup_rem = cmds.duplicate(remaining)
-	for j in range(100):
-		# 100 tries for a suitable part
+	bbox_rem = cmds.exactWorldBoundingBox(remaining)
+	for j in range(30):
+		# get the cutpart at right position
 		cutpart = create_part(part[0])
-		vol_part = volume(cutpart)
 		cutcenter = rand_center(remaining)
 		move_obj_center(cutpart, cutcenter)
 
-		new_cut = cmds.polyBoolOp(cmds.duplicate(remaining), cmds.duplicate(cutpart), op=3)[0]
+		vol_part = volume(cutpart)
+		bbox_cut = cmds.exactWorldBoundingBox(cutpart)
+
+		# percentage inside check
+		pi = vertices_outside(bbox_rem, bbox_cut)
+		if pi < 5:
+			cmds.delete(cutpart)
+			continue
+
+		# volumn check
+		dum_rem = cmds.duplicate(remaining)
+		dum_cut = cmds.duplicate(cutpart)
+		new_cut = cmds.polyBoolOp(dum_rem, dum_cut, op=3)[0]
 		new_rem = cmds.polyBoolOp(remaining, cutpart, op=2)[0]
 		vol_cut = volume(new_cut)
-		if vol_cut / vol_part > 0.9:
+		if vol_cut / vol_part > 0.6:
 			# this part might work
 			result = rec(new_rem, part[1:], move + 3)
 			if result == True:
@@ -65,13 +97,13 @@ def rec(remaining, part, move):
 			else:
 				cmds.delete(new_cut)
 				remaining = cmds.duplicate(backup_rem)
-				print 'shit try again'
+				print 'try again in step', move
 		else:
 			cmds.delete(new_cut)
 			cmds.delete(new_rem)
 			remaining = cmds.duplicate(backup_rem)
 
-	print 'oh no fail 100 times in step', move
+	print 'fail in step', (move-5)/3
 	cmds.delete(backup_rem)
 	cmds.delete(remaining)
 	return False
@@ -82,6 +114,9 @@ cmds.select(all=True)
 cmds.delete()
 
 remaining = cmds.polyCube(h=3, w=3, d=3)[0]
+#remaining = cmds.polyCylinder(r=3, h=3, ax=[0,1,0])[0]
+#remaining = cmds.polyCone(r=3, h=3, ax=[0,1,0])[0]
+#cube1 = cmds.polyPyramid()
 part0 = [-1, -1, -1, 1, 1, 1]
 part1 = [-0.5, -1, -1.5, 0.5, 1, 1.5]
 part2 = [-0.5, -1, -1.5, 0.5, 1, 1.5]
@@ -91,14 +126,19 @@ rec(remaining, part[:4], 5)
 
 # cutpart = create_part(part0)
 # vp = volume(cutpart)
-# cutcenter = [0,0,0]
+# cutcenter = [1,1,0]
 # move_obj_center(cutpart, cutcenter)
+
+# minx, miny, minz, maxx, maxy, maxz = cmds.exactWorldBoundingBox(remaining)
+# cminx, cminy, cminz, cmaxx, cmaxy, cmaxz = cmds.exactWorldBoundingBox(cutpart)
+
+# print percentage_inside_check(cmds.exactWorldBoundingBox(cutpart), cmds.exactWorldBoundingBox(remaining))
+
 # dup_rem = cmds.duplicate(remaining)
 # dup_cut = cmds.duplicate(cutpart)
 # new_rem = cmds.polyBoolOp(remaining, cutpart, op=2)[0]
 # new_cut = cmds.polyBoolOp(dup_rem, dup_cut, op=3)[0]
-# vc = volume(new_cut)
-# print vc/vp
+
 
 # reunion
 # ew_rem = cmds.polyBoolOp(new_rem, new_cut, op=1)[0]
